4333a49a5c94912aa15e21d40ee883d6b6faa318
Stephen Collins <stephenc211@gmail.com>
2018-02-09 12:52:17 -0600

    adjustment for PR changes requested

--- desktop/src/gameover_scene_slc.nim
+++ desktop/src/gameover_scene_slc.nim
@@ -3,14 +3,6 @@ import scene_management
 from game_input import wasClicked
 from text_renderer import renderTextCached
 
-proc drawGameOverText(game: Game) =
-  # draw the game over scene text
-  let
-    coins: string = $game.state.coins
-
-  game.renderTextCached("TIME'S UP!", 560, 340, WHITE )
-  game.renderTextCached("SCORE: " & coins, 567, 375, WHITE )
-
 proc registerGameoverScene(scene: Scene, game: Game, tick:int) =
   # load assets here
   echo "registering gameover scene"
@@ -26,7 +18,11 @@ proc updateGameoverScene(scene: Scene, game: Game, tick:int) =
 
 proc renderGameoverScene(scene: Scene, game: Game, tick:int) =
   # called on game render proc
-  game.drawGameOverText()
+  let
+    coins: string = $game.state.coins
+
+  game.renderTextCached("TIME'S UP!", 560, 340, WHITE )
+  game.renderTextCached("SCORE: " & coins, 567, 375, WHITE )
 
 proc exitGameoverScene(scene: Scene, game: Game, tick:int) =
   # exit animation / leave gameover scene here
